d586d81dc52b89b412bf5ee9f9a56866d5e7d4b8
Igor Artamonov <igor@artamonov.ru>
2018-06-14 22:47:01 -0400

    solution: improve peer discover, fast mode is optional

--- docker/geth/start.sh
+++ docker/geth/start.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+set -e
+
+EXTERNAL_IP=$(curl ifconfig.co)
+
 OPTS=""
 OPTS="$OPTS --datadir /data"
 OPTS="$OPTS --rpc --rpcaddr 0.0.0.0"
 OPTS="$OPTS --max-peers 100"
-OPTS="$OPTS --fast"
+OPTS="$OPTS --nat extip:$EXTERNAL_IP"
+#OPTS="$OPTS --cache 2048"
+
+if [ "$MODE" = "fast" ]; then
+    OPTS="$OPTS --fast"
+fi
 
 echo "-------------------------------------------------------"
 echo "Options: $OPTS"
